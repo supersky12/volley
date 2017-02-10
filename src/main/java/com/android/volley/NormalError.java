da00fe77ddcac1989be0e2f4d4515c597e28cff6
xieb@133.cn <xieb123@gmail.com>
2017-02-10 20:57:09 +0800

    Rejected commit: Default

---
+++ src/main/java/com/android/volley/NormalError.java
@@ -0,0 +1,12 @@
+package com.android.volley;
+
+/**
+ * @author xieb
+ * @date 17/2/10
+ */
+
+public class NormalError extends VolleyError {
+    public NormalError(String exceptionMessage) {
+        super(exceptionMessage);
+    }
+}
